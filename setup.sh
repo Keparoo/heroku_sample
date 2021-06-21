5b8f4c645644579b771f43ad88f72a36a183bbd7
Kep <KepCreations@gmail.com>
2021-06-21 18:15:19 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-DATABASE_URL='postgresql+psycopg2://kep:password@127.0.0.1:5432/herokusample'
-EXCITED='true'
+DATABASE_URL=postgresql+psycopg2://kep:password@127.0.0.1:5432/herokusample
+EXCITED=true
