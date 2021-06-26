0c106000a0ff8c8e083283432d73af915d59ecf3
Kep <KepCreations@gmail.com>
2021-06-26 11:26:55 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-DATABASE_URL=postgresql://kep:password@127.0.0.1:5432/herokusample
+DATABASE_URL=postgresql+psycopg2://kep:password@127.0.0.1:5432/herokusample
 EXCITED=true
+FLASK_APP=app.py
+FLASK_ENV=development
